3bf60c8d337ae6e2f50aa3b8df63cbcad5731f40
ganesh5516 <ganesh.ghule@vvdntech.in>
2019-05-07 11:58:08 +0530

    Create the list and add the elements

---
+++ 3.2.4.py
@@ -0,0 +1,8 @@
+#Create the list and add the elements
+
+Employee = ['Ganesh','Shohib','Himanshu','Shankeey']
+n =int(input("Enter How many Name We Want To Add :  "))
+for i in range(n):
+	Employee[4:n] = [raw_input('')]
+
+print(Employee)
