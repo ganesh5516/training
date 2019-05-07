e31b616947a6bf94ef8ac8849c0a93e2e07852ee
ganesh5516 <ganesh.ghule@vvdntech.in>
2019-05-07 11:38:49 +0530

    convert input into different data type

---
+++ 1.1.3.py
@@ -0,0 +1,14 @@
+#Get the input from user and convert it into different data types and print it
+
+z = input("Enter The Input : ")
+print(type(z))
+
+x = str(z)
+print(type(x))
+
+y = float(x) 
+print(type(z))
+
+
+t = bool(y)
+print(type(t))
